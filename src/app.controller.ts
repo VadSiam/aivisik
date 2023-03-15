4613f0ac0a74fb4d232d1aa80736e37fe1cba236
Vad_Siam <nevod80@gmail.com>
2023-03-15 13:39:17 +0700

    add cron function to generate every 8 hours

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  // @Get()
   // getImage() {
   //   return this.appService.getOpenAIImage();
   // }
@@ -18,9 +18,10 @@ export class AppController {
   // getFB() {
   //   return this.appService.getFBCall();
   // }
-  getAndDownloadImage() {
-    return this.appService.getAIImage();
-  }
+  // MAIN FUNCTION
+  // getAndDownloadImage() {
+  //   return this.appService.getAIImageAndPostToInsta();
+  // }
 
   // getHumanityStats() {
   //   return this.appService.getHumanityStats();
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, ScheduleModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
